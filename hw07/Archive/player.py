ca6ab8760b170c2e3ffbbe8543bf9dd1d1ec3aa9
Jane Wan <janewan@Janes-MacBook-Air.local>
2023-11-01 18:49:26 -0700

    Added hw07

---
+++ hw07/Archive/player.py
@@ -0,0 +1,11 @@
+#Fangyuan Wan
+#A dodgeball team management system
+
+class Player:
+    """
+    A class representing a dodgeball player
+    """
+    def __init__(self, name, number, position):
+        self.name = name
+        self.number = number
+        self.position = position
---
+++ hw07/luhn.py
@@ -0,0 +1,34 @@
+# Fangyuan Wan
+# Luhn's algorithm
+
+def modify_digits(nums):
+    """
+    Determines whether or not the number
+    is valid using Luhn’s algorithm.
+    """
+    digits = [int(digit) for digit in str(nums)]
+
+    for i in range(len(digits)-2, -1, -2):
+        doubled = digits[i] * 2
+        if doubled >= 10:
+            digits[i] = doubled//10 + doubled % 10
+        else:
+            digits[i] = doubled
+
+    total = sum(digits)
+    print(total)
+
+    if total % 10 == 0:
+        print(f"{nums} is vaild")
+    else:
+        print(f"{nums} is not vaild")
+
+def main():
+    """
+    Asks for an account number and calls the function
+    """
+    nums = input("Enter an account number: ")
+
+    modify_digits(nums)
+
+main()
